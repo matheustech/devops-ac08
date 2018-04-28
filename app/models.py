f8456bec3f8c18f9d165ebfcfe1b85113b1b0d8b
Thiago Timoteo da Silva <33885056+thiagothimas@users.noreply.github.com>
2018-04-28 14:14:17 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,5 +13,5 @@ class Curso(models.Model):
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
     
-class Candidatos(models.Model):
+class Candidato(models.Model):
     nome = models.CharField(max_length=100)
